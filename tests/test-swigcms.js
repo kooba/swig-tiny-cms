5ce59819dc1c17086136138a3ce46ca15ba533f2
Jakub Borys <jakub.borys@gmail.com>
2014-04-29 14:26:24 +0100

    Making test less brittle. Playing with connect-inject

--- tests/test-swigcms.js
+++ tests/test-swigcms.js
@@ -30,23 +30,40 @@ app.use(function (req, res, next) {
 
 swigCms.initialize(swig, app, options);
 
-describe('When adding Swig CMS tag to a page', function() {
-  describe('and content id is not provided', function() {
+describe('Swig CMS test suite', function() {
 
-    before(function (done) {
-      createViewFile('index.html', "{% cms %}");
-      app.get('/', function (req, res) {
-        res.render('index', {});
+  //Cleanup any existing test files
+  before(function(done) {
+
+    var contentDirectory = path.join(options.root, options.contentDirectory);
+    var viewsDirectory = path.join(options.root, 'views');
+
+    if(fs.existsSync(contentDirectory))
+      fs.rmdirSync(contentDirectory);
+
+    if(fs.existsSync(viewsDirectory))
+      fs.rmdirSync(viewsDirectory);
+
+    done();
+  });
+
+  describe('When adding Swig CMS tag to a page', function() {
+    describe('and content id is not provided', function() {
+
+      before(function (done) {
+        createViewFile('index.html', "{% cms %}");
+        app.get('/', function (req, res) {
+          res.render('index', {});
+        });
+        done();
       });
-      done();
-    });
 
-    after(function (done) {
-      removeViewFile('index.html');
-      done();
-    });
+      after(function (done) {
+        removeViewFile('index.html');
+        done();
+      });
 
-    it('error will be thrown', function(done) {
+      it('error will be thrown', function(done) {
         request(app)
           .get('/')
           .expect(500)
@@ -59,192 +76,197 @@ describe('When adding Swig CMS tag to a page', function() {
               done();
             }
           });
-    });
-  });
-});
-
-describe('When rendering cms tag for non Admin users', function () {
-  describe('content will be', function () {
-    admin = false;
-    it('blank if new', function (done) {
-      swig.render("{% cms 'test' %}").should.equal('');
-      done();
-    });
-
-    it('read from the disk if exists', function (done) {
-      createContentFile('test.md', '#test');
-      swig.render("{% cms 'test' %}").should.equal("<h1 id=\"test\">test</h1>\n");
-      removeContentFile('test.md');
-      done();
-    });
-
-  });
-});
-
-describe('When rendering cms tag for Admin user', function () {
-  describe('content will be', function () {
-    before(function (done) {
-      admin = true;
-      app.get('/', function (req, res) {
-        res.render('index', {});
       });
-      createViewFile('index.html', "{% cms '" + contentName + "' %}");
-      done();
-    });
-
-    beforeEach(function (done) {
-      swig.invalidateCache();
-      done();
-    });
-
-    after(function (done) {
-      removeViewFile('index.html');
-      done();
-    });
-
-    it('editable and blank if new', function (done) {
-      request(app)
-        .get('/')
-        .expect(200)
-        .end(function (err, res) {
-          res.text.should.equal("<a href='/swig-cms/edit/" + contentName + "?returnUrl=/'>Edit</a><div></div>")
-          done();
-        });
-    });
-
-    it('editable with content read from the disk', function (done) {
-      createContentFile(contentName + '.md', '#' + contentName);
-
-      request(app)
-        .get('/')
-        .expect(200)
-        .end(function (err, res) {
-          res.text.should.equal("<a href='/swig-cms/edit/" + contentName + "?returnUrl=/'>Edit</a><div><h1 id=\"" + contentName + "\">" + contentName + "</h1>\n</div>");
-          removeContentFile(contentName + '.md');
-          done();
-        });
     });
   });
-});
 
-describe('When editing content', function () {
-   describe('by un-authorized users', function() {
-    it('they will be redirected back', function (done) {
+  describe('When rendering cms tag for non Admin users', function () {
+    describe('content will be', function () {
       admin = false;
-      request(app)
-        .get('/' + options.route + '/edit/' + contentName + '/?returnUrl=/')
-        .expect(302)
-        .expect(/Moved Temporarily\. Redirecting to \//)
-        .end(done);
+      it('blank if new', function (done) {
+        swig.render("{% cms 'test' %}").should.equal('');
+        done();
+      });
+
+      it('read from the disk if exists', function (done) {
+        createContentFile('test.md', '#test');
+        swig.render("{% cms 'test' %}").should.equal("<h1 id=\"test\">test</h1>\n");
+        removeContentFile('test.md');
+        done();
+      });
+
     });
   });
 
-  describe('by authorized users', function() {
-    before(function(done) {
-      createContentFile(contentName + '.md', '#test');
-      done();
-    });
+  describe('When rendering cms tag for Admin user', function () {
+    describe('content will be', function () {
+      before(function (done) {
+        admin = true;
+        app.get('/', function (req, res) {
+          res.render('index', {});
+        });
+        createViewFile('index.html', "{% cms '" + contentName + "' %}");
+        done();
+      });
 
-    after(function(done) {
-      removeContentFile(contentName + '.md');
-      done();
-    });
+      beforeEach(function (done) {
+        swig.invalidateCache();
+        done();
+      });
 
-    it('editor will rendered', function(done) {
-      admin = true;
-      request(app)
-        .get('/' + options.route + '/edit/' + contentName)
-        .expect(200)
-        .expect(/#test/)
-        .end(done);
-    });
-  });
-});
+      after(function (done) {
+        removeViewFile('index.html');
+        done();
+      });
 
-describe('When saving content', function () {
-  describe('by authorized users', function() {
-    it('new content will be saved', function(done) {
+      it('editable and blank if new', function (done) {
+        request(app)
+          .get('/')
+          .expect(200)
+          .end(function (err, res) {
+            res.text.should.equal("<a href='/swig-cms/edit/" + contentName + "?returnUrl=/'>Edit</a><div></div>")
+            done();
+          });
+      });
 
-      admin = true;
-      request(app)
-        .post('/' + options.route + '/save/?returnUrl=/')
-        .type('form')
-        .send({ content: '#test' })
-        .send({ contentId: 'test' })
-        .expect(302)
-        .expect(/Moved Temporarily\. Redirecting to \//)
-        .end(function (err, res) {
-          if(err) {
-            done(err);
-          } else {
-            readContentFile(contentName + '.md').should.equal('#test');
+      it('editable with content read from the disk', function (done) {
+        createContentFile(contentName + '.md', '#' + contentName);
+
+        request(app)
+          .get('/')
+          .expect(200)
+          .end(function (err, res) {
+            res.text.should.equal("<a href='/swig-cms/edit/" + contentName + "?returnUrl=/'>Edit</a><div><h1 id=\"" + contentName + "\">" + contentName + "</h1>\n</div>");
             removeContentFile(contentName + '.md');
             done();
-          }
-        });
+          });
+      });
     });
   });
 
-  describe('by un-authorized users', function() {
-    it('content won\'t be saved', function(done) {
-      admin = false;
-      request(app)
-        .post('/' + options.route + '/save/?returnUrl=/')
-        .type('form')
-        .send({ content: '#test' })
-        .send({ contentId: 'test' })
-        .expect(302)
-        .expect(/Moved Temporarily\. Redirecting to \//)
-        .end(function (err, res) {
-          if(err) {
-            done(err);
-          } else {
-            fs.existsSync(contentName + '.md').should.be.false;
-            done();
-          }
-        });
+  describe('When editing content', function () {
+    describe('by un-authorized users', function() {
+      it('they will be redirected back', function (done) {
+        admin = false;
+        request(app)
+          .get('/' + options.route + '/edit/' + contentName + '/?returnUrl=/')
+          .expect(302)
+          .expect(/Moved Temporarily\. Redirecting to \//)
+          .end(done);
+      });
+    });
+
+    describe('by authorized users', function() {
+      before(function(done) {
+        createContentFile(contentName + '.md', '#test');
+        done();
+      });
+
+      after(function(done) {
+        removeContentFile(contentName + '.md');
+        done();
+      });
+
+      it('editor will rendered', function(done) {
+        admin = true;
+        request(app)
+          .get('/' + options.route + '/edit/' + contentName)
+          .expect(200)
+          .expect(/#test/)
+          .end(done);
+      });
     });
   });
 
-  describe('with invalid parameters', function() {
-    it('will respond with error', function(done) {
-      admin = true;
-      request(app)
-        .post('/' + options.route + '/save/?returnUrl=/')
-        .type('form')
-        .expect(500)
-        .expect(/Swig CMS Error: Insufficient parameters when saving content./)
-        .end(done);
+  describe('When saving content', function () {
+    describe('by authorized users', function() {
+      it('new content will be saved', function(done) {
+
+        admin = true;
+        request(app)
+          .post('/' + options.route + '/save/')
+          .type('form')
+          .send({ content: '#test' })
+          .send({ contentId: 'test' })
+          .send({ returnUrl: '/' })
+          .expect(302)
+          .expect(/Moved Temporarily\. Redirecting to \//)
+          .end(function (err, res) {
+            if(err) {
+              done(err);
+            } else {
+              readContentFile(contentName + '.md').should.equal('#test');
+              removeContentFile(contentName + '.md');
+              done();
+            }
+          });
+      });
+    });
+
+    describe('by un-authorized users', function() {
+      it('content won\'t be saved', function(done) {
+        admin = false;
+        request(app)
+          .post('/' + options.route + '/save/?returnUrl=/')
+          .type('form')
+          .send({ content: '#test' })
+          .send({ contentId: 'test' })
+          .expect(302)
+          .expect(/Moved Temporarily\. Redirecting to \//)
+          .end(function (err, res) {
+            if(err) {
+              done(err);
+            } else {
+              fs.existsSync(contentName + '.md').should.be.false;
+              done();
+            }
+          });
+      });
+    });
+
+    describe('with invalid parameters', function() {
+      it('will respond with error', function(done) {
+        admin = true;
+        request(app)
+          .post('/' + options.route + '/save/?returnUrl=/')
+          .type('form')
+          .expect(500)
+          .expect(/Swig CMS Error: Insufficient parameters when saving content./)
+          .end(done);
+      });
     });
   });
+
+  describe('When content is changed by other process', function() {
+    it('current process will reload it from the disk', function(done) {
+      done();
+    });
+  });
+
+  describe('Default options will be used if not provided', function() {
+    it('for root', function(done) {
+      done();
+    });
+    //etc...
+  });
+
+  describe('Options can be customized:', function() {
+    it('Controller slug can be changed', function(done) {
+      done();
+    });
+
+    it('Content directory can be changed', function(done) {
+      done();
+    });
+
+    it('Marked options can be changed', function(done) {
+      done();
+    });
+  });
+
 });
 
-describe('When content is changed by other process', function() {
-  it('current process will reload it from the disk', function(done) {
-    done();
-  });
-});
 
-describe('Default options will be used if not provided', function() {
-  it('for root', function(done) {
-    done();
-  });
-  //etc...
-});
-
-describe('Options can be customized:', function() {
-  it('Controller slug can be changed', function(done) {
-    done();
-  });
-
-  it('Content directory can be changed', function(done) {
-    done();
-  });
-
-  it('Marked options can be changed', function(done) {
-    done();
-  });
-});
 
 var createContentFile = function (name, content) {
   fs.mkdirSync(path.join(options.root, options.contentDirectory));
